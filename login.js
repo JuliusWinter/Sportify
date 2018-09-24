6e41d95ce7068bfb35f4320ead7c76c53e15b07d
Julius Winter <42949222+JuliusWinter@users.noreply.github.com>
2018-09-24 14:38:52 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+//Introduce User with credentials
+var user = {
+    userName: "Jesus",
+    password: "helloworld"
+    
+};
+
+//Select Button
+var submit = document.getElementById('submit');
+
+//On click run a function
+submit.onclick = function(){
+    //safe what ever is inside the input fields
+    var userName = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    //check if the input equals the credentials and change 
+    if(userName == user.userName && password == user.password){
+        document.getElementById("loginResult").innerHTML = "Yeah, your credentials are right!!!";
+        document.location.href = "register.html";
+    }
+    //If credentials not match show error message
+    else{
+        document.getElementById("loginResult").innerHTML = "Oops, username or password is wrong...try again!!!"
+    }
+};
+
+//
+
+
+
