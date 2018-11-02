bfdec2f810be912c069cf141f11317d79578f24c
MarcelJas <maja18ak@student.cbs.dk>
2018-11-02 17:55:30 +0100

    Search Bar Sport Cat

--- assets/js/eventCatalogue.js
+++ assets/js/eventCatalogue.js
@@ -1,10 +1,10 @@
 //define a function that searches for event categories and displays only applicable events//
 function categorySearchFunction (){
   //declare variables - getting values from search box//
-  var searchInputCat = document.getElementById('userCategoryInput').value.toUpperCase();
+  let searchInputCat = document.getElementById('userCategoryInput').value.toUpperCase();
   //Declare variables - getting values from the div elements
-  var catItems = document.getElementById('catalogueItems');
-  var events = catItems.getElementsByClassName('event');
+  let catItems = document.getElementById('catalogueItems');
+  let events = catItems.getElementsByClassName('event');
 
   //loop through the divs to search for elements, and hide those that do not match the search query//
   for (var i=0; i<events.length; i++){
@@ -21,10 +21,10 @@ function categorySearchFunction (){
 //define a function that searches for event categories and displays only applicable events//
 function locationSearchFunction (){
   //declare variables - getting values from search box//
-  var searchInputLoc = document.getElementById('userLocationInput').value.toUpperCase();
+  let searchInputLoc = document.getElementById('userLocationInput').value.toUpperCase();
   //Declare variables - getting values from the div elements
-  var catItems = document.getElementById('catalogueItems');
-  var events = catItems.getElementsByClassName('event');
+  let catItems = document.getElementById('catalogueItems');
+  let events = catItems.getElementsByClassName('event');
 
   //loop through the divs to search for elements, and hide those that do not match the search query//
   for (var i=0; i<events.length; i++){
@@ -55,30 +55,47 @@ for (i = 0; i < coll.length; i++) {
 }
 
 
-//create an array including all sports categories
+//create an function that creates a div, including a checkbox and an individual label for each array value
 
 var select = document.getElementById("content"); 
-var options = ['American Football', 'Athletics','Badminton','Basketball','Boxing ','Canoeing','Cricket','Cross-Fit','Cycling ','Dancing','Darts','Disability Sports','Diving','Fitness-Training','Football','Golf','Handball','Hiking','Hockey','Ice Hockey','Longboarding','Mixed Martial Arts','Modern Pentathlon','Motor Sports','Netball','Parkour','Rowing','Rugby','Running','Sailing','Shooting','Skateboarding','Skiing','Snooker','Snowboarding','Squash','Surfing','Swimming','Table Tennis','Tai Chi','Tennis','Triathlon','Tricking','Ultimate Frisbee','Volleyball','Weightlifting','Winter Sports','Wrestling','Yoga'];
+var sports = ['American Football', 'Athletics','Badminton','Basketball','Boxing ','Canoeing','Cricket','Cross-Fit','Cycling ','Dancing','Darts','Disability Sports','Diving','Fitness-Training','Football','Golf','Handball','Hiking','Hockey','Ice Hockey','Longboarding','Mixed Martial Arts','Modern Pentathlon','Motor Sports','Netball','Parkour','Rowing','Rugby','Running','Sailing','Shooting','Skateboarding','Skiing','Snooker','Snowboarding','Squash','Surfing','Swimming','Table Tennis','Tai Chi','Tennis','Triathlon','Tricking','Ultimate Frisbee','Volleyball','Weightlifting','Winter Sports','Wrestling','Yoga'];
 
-
-//     var el = document.createElement("");
-//     el.textContent = opt;
-//     el.value = opt;
-//     select.appendChild(el);
-// }
-
-for(var i = 0; i < options.length; i++) {
-    var opt = options[i];
+for(var i = 0; i < sports.length; i++) {
+    var opt = sports[i];
     var div = document.createElement('DIV');
     div.setAttribute('class', 'checkboxCat');
+    div.setAttribute('id', sports[i]);
+    div.setAttribute('value', sports[i]);
     var para = document.createElement("INPUT");
     para.setAttribute("type", "checkbox",);
-    para.setAttribute('id', options[i]);
-    var lab = document.createElement('LABEL');
-    lab.setAttribute('for', options[i]);
-    lab.innerHTML = options[i];
+    para.setAttribute('id', sports[i]);
+    var lab = document.createElement('LABEL'); 
+    lab.setAttribute('for', sports[i]);
+    lab.innerHTML = sports[i];
     var element = document.getElementById("content");
     element.appendChild(div);
     element.appendChild(para);
     element.appendChild(lab);
 }
+
+//define a function that searches for event categories and displays only applicable events//
+function catSearch (){
+  //declare variables - getting values from search box//
+  let searchInputCat = document.getElementById('categorySearch').value.toUpperCase();
+  //Declare variables - getting values from the div elements
+  let catItems = document.getElementById('content');
+  let category = catItems.getElementsByClassName('checkboxCat');
+
+  //loop through the divs to search for elements, and hide those that do not match the search query//
+  for (var i=0; i<category.length; i++){
+    var cat = category[i].value;
+    
+    if(cat[0].innerHTML.toUpperCase().includes(searchInputCat)){
+      category[i].style.display ="";
+    }else{
+      category[i].style.display ="none";
+    }
+  }
+}
+
+
