b25c381a19b78b47e20bc6ec6b880f6335a33049
Julius Winter <julius.e.winter@gmail.com>
2018-11-07 12:22:10 +0100

    Update assets/js/createCourse.js

--- assets/js/createCourse.js
+++ assets/js/createCourse.js
@@ -4,7 +4,8 @@ var events = [];
 
 // introduce our event object model
 class Event {
-    constructor(_type, _privacy, _name, _date, _time, _sportType, _description, _difficulty, _maxPart, _frequency, _location, _price){
+    constructor(_eventID, _type, _privacy, _name, _date, _time, _sportType, _description, _difficulty, _maxPart, _frequency, _location, _price){
+        this.eventID = _eventID;
         this.type = _type;
         this.privacy = _privacy;
         this.name = _name;
@@ -35,17 +36,34 @@ class Event {
 // var eventPrice = document.getElementById("eventPrice");
 // var eventSubmitButton = document.getElementById("submit");
 
+// Unique Event ID Generator Function
+function geid() {
+    function s4() {
+        // Math.floor -> Round Number
+        // Math.random -> generate random Number between 0 and 1
+        // *0x1000 gives us a 4 digit number
+      return Math.floor((1 + Math.random())* 0x10000)
+        // toString(16)-> gives us a mix of numbers and char
+        .toString(16)
+        // tosubstring(1) shortens the random string to 3 char
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
 // add click event listener
 // on click trigger function
 document.getElementById("eventForm").addEventListener("submit", function(event){
     // Prevent the page to automatically push the input into the URL and prevent the page to reload
     event.preventDefault();
+    // create unique event id
+    var eventID = geid();
     // check if all fields are filled out
     if(privacy && name && date && sportType){
 //     // if yes -> get all values of the fields and push them to the event object
     var eventType = "course"
     // // push new event to events array
-    events.push(new Event(eventType, event.target.privacyDropdown.value, event.target.eventName.value, event.target.eventDate.value, event.target.eventTime.value, event.target.eventSportType.value, event.target.eventDescription.value, event.target.eventDifficulty.value, event.target.eventMaxPart.value, event.target.eventFrequency.value, event.target.eventLocation.value, event.target.eventPrice.value));
+    events.push(new Event(eventID, eventType, event.target.privacyDropdown.value, event.target.eventName.value, event.target.eventDate.value, event.target.eventTime.value, event.target.eventSportType.value, event.target.eventDescription.value, event.target.eventDifficulty.value, event.target.eventMaxPart.value, event.target.eventFrequency.value, event.target.eventLocation.value, event.target.eventPrice.value));
         console.log(events);
     // // store stringified version of events array in localStorage
     localStorage.setItem("events", JSON.stringify(events));
