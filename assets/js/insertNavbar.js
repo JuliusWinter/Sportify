48afbe31febbb8672f361d5c151c6eebb78c08e9
Julius Winter <julius.e.winter@gmail.com>
2018-11-07 17:04:23 +0100

    Update insertNavbar.js

--- assets/js/insertNavbar.js
+++ assets/js/insertNavbar.js
@@ -1,5 +1,5 @@
 // Select the 
 var navbar = document.querySelector(".wrapper");
 // add the following html after the opening/begin of the div with class wrapper
-navbar.insertAdjacentHTML("afterbegin", "<nav><div class='logo'>SPORTIFY</div><ul><li><a href='index.html'>HOME</a></li><li><a href='learnMore.html'>LEARN MORE</a></li><li><a href='about.html'>ABOUT</a></li><li><a id='registerBtn' href='register.html'>REGISTER</a></li><li><a id='loginBtn' class='active' href='login.html'>LOG IN</a></li></ul></nav>");
+navbar.insertAdjacentHTML("afterbegin", "<nav id='navbar'><div class='logo'>SPORTIFY</div><ul><li><a href='index.html'>HOME</a></li><li><a href='learnMore.html'>LEARN MORE</a></li><li><a href='about.html'>ABOUT</a></li><li><a id='registerBtn' href='register.html'>REGISTER</a></li><li><a id='loginBtn' class='active' href='login.html'>LOG IN</a></li></ul></nav>");
 
