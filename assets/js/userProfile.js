76f02419584b9392b414369fa5caf7225dd1dcc6
Georget93 <getr18ab@student.cbs.dk>
2018-11-07 18:34:26 +0100

    UserProfile,
    Change in user profile and centeralized css into login.css

--- assets/js/userProfile.js
+++ assets/js/userProfile.js
@@ -1,3 +1,31 @@
+// Get users array from local Storage for authentification
+
+
+
+
+// // Where does this event come from and what is it?
+// document.getElementById("loginForm").addEventListener("submit", function(event){
+//     // Prevent the page to automatically push the input into the URL and prevent the page to reload
+//     event.preventDefault();
+//     // loop over users array and check if credentials match a registered user
+//     for(var i = 0; i < users.length; i++){
+//         if(event.target.username.value == users[i].userName && event.target.password.value == users[i].password){
+//             // if true change isLoggedIn attribute to true
+//             users[i].isLoggedIn = true;
+//             // push user to currentUser array
+//             currentUser.push(users[i].push);
+//             // Safe stringified currentUser array to local storage
+//             localStorage.setItem("currentUser", JSON.stringify(currentUser));
+//             // redirect to user profile
+//             document.location.href = "userProfile.html";
+//         }
+//         else{
+//             document.getElementById("loginResult").innerHTML = "Oops, username or password is wrong...try again!!!"
+//         }
+//     }
+// })
+
+
 
 //Search function applied to table in profile page, filtering all created events by current user
 function searchCatalogueTable() {
@@ -20,11 +48,8 @@ function searchCatalogueTable() {
       } 
     }
   }
-
-
-//User first and last name entered into header via userName
-var users = JSON.parse(localStorage.getItem("users"));
-
+  //import current user values
+var currentUser = JSON.parse(localStorage.getItem("currentUser"));
 //set variables for button ID's
 var btnUpcoming = document.getElementById("upcomingEventsBtn");
 var btnPast = document.getElementById("pastEventsBtn");
@@ -33,6 +58,16 @@ var btnInterests = document.getElementById("interestsBtn");
 var upcomingEventsDIV = document.getElementById("upcomingEvents");
 var pastEventsDIV= document.getElementById("pastEvents");
 var interestsDIV = document.getElementById("interests");  
+
+var inActive = document.getElementById("inactive");
+// var btnUpcoming, btnPast, btnInterests, upcomingEventsDIV, pastEventsDIV, interestsDIV;
+// btnUpcoming = document.getElementById("upcomingEventsBtn");
+// btnPast = document.getElementById("pastEventsBtn");
+// btnInterests = document.getElementById("interestsBtn");
+// upcomingEventsDIV = document.getElementById("upcomingEvents");
+// pastEventsDIV = document.getElementById("pastEvents");
+// interestsDIV = document.getElementById("interests");
+
 
 //function for first button in upcoming events
 btnUpcoming.addEventListener("click", function(){
@@ -68,3 +103,23 @@ btnInterests.addEventListener("click", function(){
         pastEventsDIV.style.display = "none";
     }
 });
+//logout function
+inActive.addEventListener("click", function(){
+if (currentUser.isLoggedIn = true ) {
+    currentUser.isLoggedIn = false ;
+    document.location.href = "login.html";
+} else {
+  interestsDIV.style.display = "block";
+    currentUser.isLoggedIn = false ;
+    document.location.href = "login.html";
+    alert("User has been logged out");
+}
+});
+
+// Access first name of first User and last name of first User
+document.getElementById("localUserName").innerHTML = "Hello " + currentUser[0].firstName + " " +currentUser[0].lastName;
+
+// Access location from reg form, not implemented yet :::  currentUser[0].location;
+document.getElementById("userLocation").innerHTML = "Sample Location, inserted once implemeneted";
+// Access email of first User
+document.getElementById("null").innerHTML = currentUser[0].email;
