48e333ce878095d9eed5d94a44859dbd19e89c7f
Julius Winter <julius.e.winter@gmail.com>
2018-11-07 17:38:08 +0100

    Fixes on navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,20 @@
+// Get current user data from local storage
+var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+// select anchor tags that should be manipulated
+var registerBtn = document.querySelector("#registerBtn");
+var loginBtn = document.querySelector("#loginBtn");
+var logoutBtn = document.querySelector("#logoutBtn");
+// check if a user is logged in
+if(currentUser){
+    registerBtn.style.display = "none";
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "inline";
+}
+else{
+    registerBtn.style.display = "inline";
+    loginBtn.style.display = "inline";
+    logoutBtn.style.display = "none";
+}
 //when user clicks Sportify logo, jump to top of index page//
 //when user clicks on Products, jump to second section//
 //when user clicks on About us, jump to third section//
