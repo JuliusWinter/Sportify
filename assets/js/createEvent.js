a5c3f0919a2d3d656f79b2094e107a485cf03d4b
Julius Winter <julius.e.winter@gmail.com>
2018-11-11 16:34:53 +0100

    commit

--- assets/js/createEvent.js
+++ assets/js/createEvent.js
@@ -12,13 +12,69 @@ if(!JSON.parse(localStorage.getItem("events"))){
 }else{
     var events = JSON.parse(localStorage.getItem("events"));
 }
-// Activate Google Places API autocomplete
-function activatePlacesSearch(){
-    // Create the autocomplete object, restricting the search to geographical
-    // location types.
-    var input = document.getElementById("eventLocation");
-    var autocomplete = new google.maps.places.Autocomplete(input);
+
+
+
+
+
+
+
+
+var placeSearch, autocomplete;
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+     (document.getElementById('eventLocation')),
+      {types: ['geocode']});
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
 }
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  console.log(place);
+
+//   for (var component in componentForm) {
+//     document.getElementById(component).value = '';
+//     document.getElementById(component).disabled = false;
+//   }
+
+  // Get each component of the address from the place details
+  // and fill the corresponding field on the form.
+//   for (var i = 0; i < place.address_components.length; i++) {
+//     var addressType = place.address_components[i].types[0];
+//     if (componentForm[addressType]) {
+//       var val = place.address_components[i][componentForm[addressType]];
+//       document.getElementById(addressType).value = val;
+//     }
+//   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // select anchor tags that should be manipulated
 var userProfile = document.querySelector("#userProfile");
@@ -123,7 +179,6 @@ function setMinDate (){
 document.getElementById("eventForm").addEventListener("submit", function(event){
     // Prevent the page to automatically push the input into the URL and prevent the page to reload
     event.preventDefault();
-    console.log(getPlace());
     // generate unique event ID
     var eventID = geid();
     var creatorID = currentUser[0].ID;
